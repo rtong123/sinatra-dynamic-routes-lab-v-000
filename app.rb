c53e0b3ed475778e1c114b223424bf43a736518b
Rebecca Tong <rtong@buffalo.edu>
2018-07-02 17:57:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,6 +30,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    binding.pry
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
