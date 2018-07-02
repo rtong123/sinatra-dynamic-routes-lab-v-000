b5e9d8ad9713262193bb3fe1d66666e153d16298
Rebecca Tong <rtong@buffalo.edu>
2018-07-02 17:59:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -43,7 +43,7 @@ class App < Sinatra::Base
     when "multiply"
       return "#{@number1.to_i * @number2.to_i}"
     else "divide"
-    return @number1.to_i / @number2.to_i
+    return "#{@number1.to_i / @number2.to_i}"
     end
 
 
